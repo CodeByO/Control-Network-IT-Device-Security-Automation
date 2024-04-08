ab139170121f4935ee1dbe04a320f0ccba47bc0c
김건희 <101076201+geonheek@users.noreply.github.com>
2024-04-08 17:46:55 +0900

    입력화면 UI

--- interface/tabs.py
+++ interface/tabs.py
@@ -1,5 +1,10 @@
+# [Title] LoginWindow
+# [DESC] 접속 정보 입력 위젯 생성
+# [Writer] geonheek, yuu4172
+
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QTabWidget, QWidget
+from PyQt5.QtWidgets import QVBoxLayout, QComboBox, QLineEdit, QApplication, QMainWindow, QTabWidget, QPushButton, QWidget
+from PyQt5.QtCore import Qt
 
 # [Func] VulnerabilityCheckTab
 # [DESC] 취약점 점검 탭 위젯 생성
@@ -7,11 +12,57 @@ from PyQt5.QtWidgets import QApplication, QMainWindow, QTabWidget, QWidget
 # [ISSUE] None
 def VulnerabilityCheckTab():
     """
-    취약점 점검 탭 위젯 생성
+    취약점 점검 탭 위젯 생성, 접속 정보 입력 UI 추가
     :return: 생성된 취약점 점검 탭 위젯
     """
     vulnerability_check_tab = QWidget()
+    layout = QVBoxLayout(vulnerability_check_tab)
+    
+    # 접속 방식 선택 드롭다운
+    ''
+    connectionTypeComboBox = QComboBox()
+    connectionTypeComboBox.addItem("접속 방식 선택") 
+    connectionTypeComboBox.addItem("Windows")
+    connectionTypeComboBox.addItem("Linux")
+    connectionTypeComboBox.setPlaceholderText("접속 방식 선택")  
+    layout.addWidget(connectionTypeComboBox)
+    
+    # 시스템 IP 주소 입력
+    ipLineEdit = QLineEdit()
+    ipLineEdit.setPlaceholderText("시스템 IP 주소")  
+    layout.addWidget(ipLineEdit)
+    
+    # 포트 번호 입력
+    portLineEdit = QLineEdit()
+    portLineEdit.setPlaceholderText("포트 번호")  
+    layout.addWidget(portLineEdit)
+    
+    # ID 입력
+    idLineEdit = QLineEdit()
+    idLineEdit.setPlaceholderText("ID")
+    layout.addWidget(idLineEdit)
+    
+    # Password 입력
+    passwordLineEdit = QLineEdit()
+    passwordLineEdit.setEchoMode(QLineEdit.Password)  
+    passwordLineEdit.setPlaceholderText("Password")  
+    layout.addWidget(passwordLineEdit)
+
+    # '>' 버튼 추가
+    nextButton = QPushButton(">")
+    nextButton.setFixedSize(30, 30)
+    nextButton.clicked.connect(lambda: onNextButtonClicked())  # '>' 버튼 클릭 시 호출될 함수
+    
+    # 버튼을 오른쪽으로 정렬하여 레이아웃에 추가
+    layout.addWidget(nextButton, alignment=Qt.AlignRight)
+    
     return vulnerability_check_tab
+
+def onNextButtonClicked():
+    """
+    '>' 버튼 클릭 시 호출될 함수
+    """
+    print("규제 지침 선택 화면으로 넘어가는 로직 구현")
 
 # [Func] InspectionHistoryTab
 # [DESC] 점검 이력 조회 탭 위젯 생성
@@ -50,6 +101,7 @@ def SetUpTabs():
             } 
         """)
     return tab_widget
+
 
 # [Func] main
 # [DESC] 애플리케이션 메인 함수
