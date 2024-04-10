e44c704f7a41ec6f40b04190d1b360c82985ce3a
김건희 <101076201+geonheek@users.noreply.github.com>
2024-04-10 21:52:52 +0900

    규제 지침 조회 UI 구현 및 기능 구현

--- interface/tabs.py
+++ interface/tabs.py
@@ -8,9 +8,20 @@ import sys
 import sqlite3
 from pathlib import Path
 
-from PyQt5.QtWidgets import QVBoxLayout, QComboBox, QLineEdit, QApplication, QMainWindow, QTabWidget, QPushButton, QWidget, QTabBar, QMessageBox
+from PyQt5.QtWidgets import (QVBoxLayout, QComboBox, QLineEdit, QApplication, QMainWindow,
+ QTabWidget, QPushButton, QWidget, QTabBar, QMessageBox, QStackedWidget,
+ QCheckBox, QTableWidget, QHeaderView, QHBoxLayout, QTableWidgetItem)
 from PyQt5.QtCore import Qt, QRect
 from PyQt5.QtGui import QPainter, QTransform
+
+sys.path.append(os.path.dirname( os.path.dirname( os.path.abspath(__file__) ) ))
+
+from module.auto_module import InspectionAutomation
+
+path_src = Path(__file__)
+path_database = path_src.parent / "AutoInspection.db"
+
+inspection_targets = list()
 
 # [CLASS] VerticalTabWidget
 # [DESC] 수직 탭 위젯을 위한 커스텀 QTabBar 구현
@@ -47,232 +58,296 @@ class VerticalTabWidget(QTabBar):
             
             painter.restore() 
 
-# [Func] VulnerabilityCheckTab
-# [DESC] 취약점 점검 탭 위젯 생성
-# [TODO] 입력 화면, 규제 지침 조회/선택 화면, 진행/결과 화면 구현
-# [ISSUE] None
-def VulnerabilityCheckTab():
-    """
-    취약점 점검 탭 위젯 생성, 접속 정보 입력 UI 추가
-    :return: 생성된 취약점 점검 탭 위젯
-    """
-    vulnerability_check_tab = QWidget()
-    layout = QVBoxLayout(vulnerability_check_tab)
-    
-    # OS 선택 드롭다운
-    OSTypeComboBox = QComboBox()
-    OSTypeComboBox.addItem("대상 OS 선택") 
-    OSTypeComboBox.addItem("Windows")
-    OSTypeComboBox.addItem("Linux")
-    OSTypeComboBox.setPlaceholderText("대상 OS 선택")  
-    layout.addWidget(OSTypeComboBox)
-    
 
-    # 접속 방식 선택 드롭다운
-    ConnectionTypeComboBox = QComboBox()
-    ConnectionTypeComboBox.addItem("접속 방식 선택") 
-    ConnectionTypeComboBox.addItem("SSH")
-    ConnectionTypeComboBox.addItem("Samba")
-    ConnectionTypeComboBox.setPlaceholderText("접속 방식 선택") 
-    layout.addWidget(ConnectionTypeComboBox)
-    
-    # 시스템 IP 주소 입력
-    ipLineEdit = QLineEdit()
-    ipLineEdit.setPlaceholderText("시스템 IP 주소")  
-    layout.addWidget(ipLineEdit)
-    
-    # 포트 번호 입력
-    portLineEdit = QLineEdit()
-    portLineEdit.setPlaceholderText("포트 번호")  
-    layout.addWidget(portLineEdit)
-    
-    # ID 입력
-    idLineEdit = QLineEdit()
-    idLineEdit.setPlaceholderText("ID")
-    layout.addWidget(idLineEdit)
-    
-    # Password 입력
-    passwordLineEdit = QLineEdit()
-    passwordLineEdit.setEchoMode(QLineEdit.Password)  
-    passwordLineEdit.setPlaceholderText("Password")  
-    layout.addWidget(passwordLineEdit)
-    
-    
-    # '>' 버튼 추가
-    nextButton = QPushButton(">")
-    nextButton.setFixedSize(30, 30)
-    nextButton.clicked.connect(lambda: onNextButtonClicked(OSTypeComboBox.currentText(), ConnectionTypeComboBox.currentText(), ipLineEdit.text(),portLineEdit.text(), idLineEdit.text(), passwordLineEdit.text()))  # '>' 버튼 클릭 시 호출될 함수
-    
-    # 버튼을 오른쪽으로 정렬하여 레이아웃에 추가
-    layout.addWidget(nextButton, alignment=Qt.AlignRight)
-    
-    
-    return vulnerability_check_tab
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('취약점 점검 시스템')
+        self.setGeometry(260, 150, 800, 500)
 
-# onNextButtonClicked 함수 정의
-def onNextButtonClicked(os_type, connection_type, ip, port, user_id, password):
-    print(os_type, connection_type, ip, port, user_id, password)
+        self.stackedWidget = QStackedWidget()
+        self.setCentralWidget(self.stackedWidget)
 
-def setupUI(main_window):
-    # '>' 버튼 생성
-    button = QPushButton(">")
-    # 버튼 클릭 시 onNextButtonClicked 함수 호출
-    # 여기서 os_type, connection_type, ip, port, id, password는 사용자 입력을 통해 얻은 값입니다.
-    # 실제 사용 시 이 값들을 적절히 얻어와야 합니다. 예제에서는 임시 값으로 대체합니다.
-    os_type = "Windows"
-    connection_type = "SSH"
-    ip = "192.168.0.1"
-    port = 22
-    user_id = "admin"
-    password = "password"
-    
-    button.clicked.connect(lambda: onNextButtonClicked(os_type, connection_type, ip, port, user_id, password))
-    
-    layout = QVBoxLayout()
-    layout.addWidget(button)
-    
-    central_widget = QWidget()
-    central_widget.setLayout(layout)
-    
-    main_window.setCentralWidget(central_widget)
+        self.mainPage = MainPage(self.stackedWidget)
 
-
-# 알림창을 띄우는 함수
-def showAlert(message):
-    msgBox = QMessageBox()
-    msgBox.setIcon(QMessageBox.Information)
-    msgBox.setText(message)
-    msgBox.setWindowTitle("경고")
-    msgBox.setStandardButtons(QMessageBox.Ok)
-    msgBox.exec_()
-
-#GUI 애플리케이션에 대한 기본 객체
-app = QApplication(sys.argv)
-
-def onNextButtonClicked(os_type, connection_type, ip, port, id, password):
-    """
-    '>' 버튼 클릭 시 호출될 함수
-    """
- 
-    if os_type == None or os_type == "대상 OS 선택":
-        showAlert("점검할 OS를 선택해 주세요")
-        return  # exit 대신 return 사용
+        self.stackedWidget.addWidget(self.mainPage)
         
-    if connection_type == None or connection_type == "접속 방식 선택":
-        showAlert("접속할 방식을 선택해 주세요")
-        return
-    ip_reg = r'^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$'
-    if not re.search(ip_reg, ip):
-        showAlert("잘못된 형식의 IP 입니다")
-        return
 
-    if not port.isdigit():
-        showAlert("숫자만 입력해 주세요")
-        return
-
-    id_reg = r'^[A-Za-z0-9_]{2,20}$'
-    if not re.search(id_reg, id):
-        showAlert("잘못된 형식의 ID 입니다")
-        return
-    
-    passwd_reg = r'^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{4,}$'
-    if not re.search(passwd_reg, password):
-        showAlert("잘못된 형식의 비밀번호 입니다")
-        return
+class MainPage(QWidget):
+    def __init__(self, stackedWidget):
+        super().__init__()
+        self.stackedWidget = stackedWidget
+        self.inspection_list_page = InspectionListPage(stackedWidget)
+        self.setup_ui()
         
-    path_src = Path(__file__)
-    path_database = path_src.parent / "AutoInspection.db"
-    
-    if not os.path.exists(path_database):
-        showAlert("DB가 존재하지 않습니다.")
-        return
+    def setup_ui(self):
+        layout = QVBoxLayout()
+        self.setLayout(layout)
 
-    VulnerabilityCheckTab()
-    
-    con = sqlite3.connect(path_database)
-    cursor = con.cursor()
-
-    try:
-        cursor.execute("SELECT PluginName, Info, Description, CommandType, ResultType from InspectionTargets WHERE TargetOS=?", (os_type, )) # SQL 명령어 실행
-    except (sqlite3.OperationalError, sqlite3.ProgrammingError):
-        showAlert("DB 실행 에러")
-        return
-    
-    inspection_targets = cursor.fetchall()
-    
-    if len(inspection_targets) == 0:
-        showAlert("대상 OS에 해당하는 규제 지침이 없습니다. 추가해주세요")
-        return
+        tab_widget = QTabWidget()
+        tab_widget.setTabBar(VerticalTabWidget())
+        vulnerability_check_tab = self.create_vulnerability_check_tab()
+        inspection_history_tab = self.create_inspection_history_tab()
+        tab_widget.addTab(vulnerability_check_tab, "취약점\n점검")
+        tab_widget.addTab(inspection_history_tab, "점검 이력\n조회")
+        tab_widget.setTabPosition(QTabWidget.West)
+        tab_widget.setStyleSheet("""
+            QTabBar::tab {
+                height: 80px;
+                width: 100px;
+                } 
+            """)
+        layout.addWidget(tab_widget)
         
-    print(inspection_targets)
-    
-    
-    con.close() # DB 연결 종료
-    
-    # 규제 지침 선택 화면 함수를 호출할 시 inspection_targets 를 인자로 주기 -> 작업 필요
-    # inspection_target에 있는 내용으로 체크 박스 선택 화면 구현 -> 작업 필요
-    
-    showAlert("규제 지침 선택 화면으로 넘어가는 로직 구현")
+    def create_vulnerability_check_tab(self):
+        vulnerability_check_tab = QWidget()
+        layout = QVBoxLayout(vulnerability_check_tab)
+        OSTypeComboBox = QComboBox()
+        OSTypeComboBox.addItem("대상 OS 선택") 
+        OSTypeComboBox.addItem("Windows")
+        OSTypeComboBox.addItem("Linux")
+        OSTypeComboBox.setPlaceholderText("대상 OS 선택")  
+        layout.addWidget(OSTypeComboBox)
 
-# [Func] InspectionHistoryTab
-# [DESC] 점검 이력 조회 탭 위젯 생성
-# [TODO] 점검 이력 조회 화면 구현
-# [ISSUE] None
-def InspectionHistoryTab():
-    """
-    점검 이력 조회 탭 위젯 생성
-    :return: 생성된 점검 이력 조회 탭 위젯
-    """
-    inspection_history_tab = QWidget()
-    return inspection_history_tab
+        ConnectionTypeComboBox = QComboBox()
+        ConnectionTypeComboBox.addItem("접속 방식 선택") 
+        ConnectionTypeComboBox.addItem("SSH")
+        ConnectionTypeComboBox.addItem("Samba")
+        ConnectionTypeComboBox.setPlaceholderText("접속 방식 선택") 
+        layout.addWidget(ConnectionTypeComboBox)
 
-# [Func] SetUpTabs
-# [DESC] 애플리케이션 내 탭 위젯 구성 및 스타일 설정
-# [TODO] 탭별 추가 기능 구현
-# [ISSUE] None
-def SetUpTabs():
-    """
-    애플리케이션 내 탭 위젯의 구성
-    :return: 구성된 QTabWidget 객체
-    """
-    # 탭 위젯 구성
-    tab_widget = QTabWidget()
-    tab_widget.setTabBar(VerticalTabWidget()) 
-    vulnerability_check_tab = VulnerabilityCheckTab()
-    inspection_history_tab = InspectionHistoryTab()
-    tab_widget.addTab(vulnerability_check_tab, "취약점\n점검")
-    tab_widget.addTab(inspection_history_tab, "점검 이력\n조회")
-    tab_widget.setTabPosition(QTabWidget.West)
+        ipLineEdit = QLineEdit()
+        ipLineEdit.setPlaceholderText("시스템 IP 주소")  
+        layout.addWidget(ipLineEdit)
+
+        portLineEdit = QLineEdit()
+        portLineEdit.setPlaceholderText("포트 번호")  
+        layout.addWidget(portLineEdit)
+
+        idLineEdit = QLineEdit()
+        idLineEdit.setPlaceholderText("ID")
+        layout.addWidget(idLineEdit)
+
+        passwordLineEdit = QLineEdit()
+        passwordLineEdit.setEchoMode(QLineEdit.Password)  
+        passwordLineEdit.setPlaceholderText("Password")  
+        layout.addWidget(passwordLineEdit)
+
+        nextButton = QPushButton(">")
+        nextButton.setFixedSize(30, 30)
+        nextButton.clicked.connect(lambda: self.on_next_button_clicked(OSTypeComboBox.currentText(), ConnectionTypeComboBox.currentText(), ipLineEdit.text(), portLineEdit.text(), idLineEdit.text(), passwordLineEdit.text()))
+        layout.addWidget(nextButton, alignment=Qt.AlignRight)
+        return vulnerability_check_tab
+
+    def create_inspection_history_tab(self):
+        return QWidget()
+
+    def on_next_button_clicked(self, os_type, connection_type, ip, port, id, password):
+            
+        # os_type = "Windows"
+        # connection_type = "SSH"
+        # ip = "192.168.0.1"
+        # port = "22"
+        # id = "admin"
+        # password = "password"
+
+        if os_type == None or os_type == "대상 OS 선택":
+            self.ShowAlert("점검할 OS를 선택해 주세요")
+            return
+
+        if connection_type == None or connection_type == "접속 방식 선택":
+            self.ShowAlert("접속할 방식을 선택해 주세요")
+            return
+        
+        ip_reg = r'^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$'
+        if not re.search(ip_reg, ip):
+            self.ShowAlert("잘못된 형식의 IP 입니다")
+            return
+
+        if not port.isdigit():
+            self.ShowAlert("숫자만 입력해 주세요")
+            return
+
+        id_reg = r'^[A-Za-z0-9_]{2,20}$'
+        if not re.search(id_reg, id):
+            self.ShowAlert("잘못된 형식의 ID 입니다")
+            return
+
+        passwd_reg = r'^(?=.*[A-Za-z\dㄱ-ㅣ가-힣])[A-Za-z\dㄱ-ㅣ가-힣]{4,}$'
+        if not re.search(passwd_reg, password):
+            self.ShowAlert("잘못된 형식의 비밀번호 입니다")
+            return
+        global inspection_targets
+        if len(inspection_targets) == 0:
+            global path_database
+
+            if not os.path.exists(path_database):
+                self.ShowAlert("DB가 존재하지 않습니다.")
+                return
+
+            con = sqlite3.connect(path_database)
+            cursor = con.cursor()
+
+            try:
+                cursor.execute("SELECT PluginName, Info, Description, CommandType, ResultType from InspectionTargets WHERE TargetOS=? AND DeleteFlag=0", (os_type, ))
+            except (sqlite3.OperationalError, sqlite3.ProgrammingError):
+                self.ShowAlert("DB 실행 에러")
+                return
+
+            inspection_targets = cursor.fetchall()
+            con.close()
+            if len(inspection_targets) == 0:
+                self.ShowAlert("대상 OS에 해당하는 규제 지침이 없습니다. 추가해주세요")
+                return
+            
+        self.inspection_list_page.SetData(inspection_targets)
+        self.inspection_list_page.SetTarget(os_type, connection_type, ip, port, id, password)
+        self.stackedWidget.addWidget(self.inspection_list_page)
+        self.stackedWidget.setCurrentIndex(1)
+        
+        #self.ShowAlert("규제 지침 선택 화면으로 넘어가는 로직 구현")
+
+    def ShowAlert(self, message):
+        msgBox = QMessageBox()
+        msgBox.setIcon(QMessageBox.Information)
+        msgBox.setText(message)
+        msgBox.setWindowTitle("경고")
+        msgBox.setStandardButtons(QMessageBox.Ok)
+        msgBox.exec_()
+
+class InspectionListPage(QWidget):
+    def __init__(self, stackedWidget):
+        super(InspectionListPage, self).__init__()
+        
+        self.stackedWidget = stackedWidget
+        
+        self.tableWidget = QTableWidget()
+        self.tableWidget.setColumnCount(7)
+        self.tableWidget.setHorizontalHeaderLabels(['선택', '이름', '설명', '실행 방식', '결과 방식', '삭제'])
+        self.tableWidget.horizontalHeader().setStretchLastSection(True)
+        self.tableWidget.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        
+        self.os_type = None
+        self.connection_type = None
+        self.ip = None
+        self.port = None
+        self.id = None
+        self.password = None
+        
+        self.initUI()
     
-    # 탭 위젯 스타일 설정
-    tab_widget.setStyleSheet("""
-        QTabBar::tab {
-            height: 80px; /* 탭의 높이 조정 */
-            width: 100px; /* 탭의 너비 조정 */ 
-            } 
-        """)
-    return tab_widget
+    def initUI(self):
+        layout = QVBoxLayout()
+        self.setLayout(layout)
+        layout.addWidget(self.tableWidget)
 
-# [Func] main
-# [DESC] 애플리케이션 메인 함수
-# [TODO] None
-# [ISSUE] None
+        # 버튼 레이아웃 생성
+        buttonLayout = QHBoxLayout()
+        
+        # 뒤로 가기 버튼 추가 및 버튼 레이아웃에 설정
+        btnBack = QPushButton("<")
+        btnBack.setFixedSize(30, 30)
+        btnBack.clicked.connect(self.goBack)
+        buttonLayout.addWidget(btnBack)  # 버튼 레이아웃에 뒤로 가기 버튼 추가
+
+        # "점검 실행" 버튼 생성 및 버튼 레이아웃에 설정
+        executeButton = QPushButton("점검 실행")
+        executeButton.setFixedSize(100, 30)  # 버튼 크기 설정
+        executeButton.clicked.connect(self.executeInspection)  # 클릭 시 executeInspection 메서드 호출
+       
+        # 버튼을 가운데로 정렬하기 위해 빈 공간 추가
+        buttonLayout.addStretch()
+        buttonLayout.addWidget(executeButton)
+        buttonLayout.addStretch()
+
+        # 버튼 레이아웃을 메인 레이아웃에 추가하여 정렬
+        layout.addLayout(buttonLayout)
+            
+    def goBack(self):
+        self.tableWidget.clearContents()
+        self.tableWidget.setRowCount(0)
+        self.stackedWidget.setCurrentIndex(0)  # 첫 번째 페이지로 돌아가기
+        
+    def executeInspection(self):
+        plugin_lists = []
+        for row in range(self.tableWidget.rowCount()):
+            chkBoxWidget = self.tableWidget.cellWidget(row, 0)
+            chkBox = chkBoxWidget.findChild(QCheckBox)
+            if chkBox.isChecked():
+                plugin_name = self.tableWidget.item(row, 6).text()
+                plugin_lists.append(plugin_name)
+        InspectionAutomation(self.os_type, self.ip, self.port, self.connection_type, self.id, self.password, plugin_lists)
+    def addRow(self, plugin, name, description, tool, action):
+        rowPosition = self.tableWidget.rowCount()
+        self.tableWidget.insertRow(rowPosition)
+
+        # 체크박스 추가
+        chkBoxWidget = QWidget()
+        chkBox = QCheckBox()
+        chkBoxLayout = QHBoxLayout(chkBoxWidget)
+        chkBoxLayout.addWidget(chkBox)
+        chkBoxLayout.setAlignment(Qt.AlignCenter)
+        chkBoxLayout.setContentsMargins(0,0,0,0)
+        self.tableWidget.setCellWidget(rowPosition, 0, chkBoxWidget)
+        # 나머지 데이터 추가
+        self.tableWidget.setItem(rowPosition, 1, QTableWidgetItem(name))
+        self.tableWidget.setItem(rowPosition, 2, QTableWidgetItem(description))
+        self.tableWidget.setItem(rowPosition, 3, QTableWidgetItem(tool))
+        self.tableWidget.setItem(rowPosition, 4, QTableWidgetItem(action))
+        
+        # 삭제 버튼 추가
+        btnDelete = QPushButton("삭제")
+        btnDelete.clicked.connect(lambda: self.deleteRow(btnDelete))
+        self.tableWidget.setCellWidget(rowPosition, 5, btnDelete)
+        self.tableWidget.setItem(rowPosition, 6, QTableWidgetItem(plugin))
+        self.tableWidget.setColumnHidden(6, True)
+
+    def deleteRow(self, button):
+        index = self.tableWidget.indexAt(button.pos())
+        if index.isValid():
+            self.tableWidget.removeRow(index.row())
+            plugin = self.tableWidget.item(index.row(), 6).text()
+            global inspection_targets
+            global path_database
+            con = sqlite3.connect(path_database)
+            cursor = con.cursor()
+            cursor.execute("SELECT TargetID from InspectionTargets WHERE PluginName=?", (plugin, ))
+            
+            target_index = cursor.fetchone()
+            target_index = target_index[0]
+            
+            inspection_targets.pop(target_index)
+            
+            cursor.execute("UPDATE from InsectionTargets SET DeleteFlag=1 WHERE PluginName=?", (plugin, ))
+            
+            con.close()
+            
+    def SetData(self, inspection_data):
+        for item in inspection_data:
+            self.addRow(item[0], item[1], item[2], item[3], item[4])
+    
+    def SetTarget(self, os_type, connection_type, ip, port, id, password):
+        self.os_type = os_type
+        self.connection_type = connection_type
+        self.ip = ip
+        self.port = port
+        self.id = id
+        self.password = password
+    
+    def ShowAlert(self, message):
+        msgBox = QMessageBox()
+        msgBox.setIcon(QMessageBox.Information)
+        msgBox.setText(message)
+        msgBox.setWindowTitle("경고")
+        msgBox.setStandardButtons(QMessageBox.Ok)
+        msgBox.exec_()
+
 def main():
-    """
-    애플리케이션의 메인 함수(애플리케이션의 초기 설정 실행)
-    """
     app = QApplication(sys.argv)
-    main_window = QMainWindow()
-    main_window.setWindowTitle('취약점 점검 시스템')
-    main_window.setGeometry(260, 150, 800, 500)
+    main_window = MainWindow()
 
-    setupUI(main_window)
-
-    tab_widget = SetUpTabs()
-    main_window.setCentralWidget(tab_widget)
     main_window.show()
     sys.exit(app.exec_())
-    
 
 if __name__ == '__main__':
     main()
